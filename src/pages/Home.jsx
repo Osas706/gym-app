2a3bcba693c357c901dab21bd0fd250be1fd8554
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-02-08 09:10:05 +0100

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState} from 'react';
+import { Box } from '@mui/material';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercise from '../components/SearchExercise';
+import Exercise from '../components/Exercise';
 
 const Home = () => {
+  const [ exercises, setExercises ] = useState([]);
+  const [ bodyPart, setBodyPart ] = useState('all');
+
   return (
-    <div>
-      
-    </div>
+    <Box>
+      <HeroBanner />
+      <SearchExercise 
+       setExercises={setExercises} 
+       bodyPart={bodyPart} 
+       setBodyPart={setBodyPart}
+      />
+      <Exercise 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}
+      />
+    </Box>
   )
 }
 
